fc1ad187b6fc6a9efcf4cf807f9e8390a43fa4f5
y9858 <53130835+y9858@users.noreply.github.com>
2025-08-31 20:55:25 +0800

    Update libwrt.sh

--- libwrt.sh
+++ libwrt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 修改默认主题
-sed -i 's/luci-theme-bootstrap/luci-theme-material/g' feeds/luci/collections/luci-light/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-light/Makefile
 
 # 修改默认 IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
